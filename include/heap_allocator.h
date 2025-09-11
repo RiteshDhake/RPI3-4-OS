681f65bf7188392f1e0fc5999921888ba060c36a
Ajinkya-25 <ajinkyabogle@gmail.com>
2025-09-11 12:57:53 +0000

    added heap aloocator files

---
+++ include/heap_allocator.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdlib.h>
+
+typedef struct heap_block {
+    size_t size;
+    struct heap_block *next;
+    int free;
+} heap_block_t;
+
+void heap_init();
+void *malloc(size_t size);      
+void free(void *ptr);
